37bc1134c116a2f902b819437162a80fb318cf2f
Tonny Bett <betttonny26@gmail.com>
2024-08-16 00:55:54 +0300

    my authenication changes

--- Kaycal interriors/src/App.jsx
+++ Kaycal interriors/src/App.jsx
@@ -1,33 +1,27 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import About from './components/About';
-import Home from './components/Home'
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css';
-import { AuthContextProvider } from "./Context/authContext";
-import DesignDisplay from './DesignDisplay';
+import ProtectedRoute from './components/ProtectedRoute'; 
+import { userAuth } from "./Context/authContext";
+
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <ProtectedRoute>
-            <About />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path='/' element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path='about' element={
+        <ProtectedRoute>
+          <About />
+        </ProtectedRoute>
+      } />
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<Signup />} />
     </Routes>
   );
 }
--- Kaycal interriors/src/main.jsx
+++ Kaycal interriors/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AuthContextProvider } from './Context/authContext.js';
+import { AuthContextProvider } from './Context/authContext.jsx';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-    ReactDOM.render(
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
         <AuthContextProvider>
             <App />
-        </AuthContextProvider>,
-        rootElement
+        </AuthContextProvider>
     );
 }
-
